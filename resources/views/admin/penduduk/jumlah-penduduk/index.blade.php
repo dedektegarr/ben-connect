9bd71d02df10f62f3f814c1d7cac2c4d1addb1c6
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2025-02-21 16:35:21 +0700

    feat: sort by desc form select periode

--- resources/views/admin/penduduk/jumlah-penduduk/index.blade.php
+++ resources/views/admin/penduduk/jumlah-penduduk/index.blade.php
@@ -208,7 +208,7 @@
                             <select id="population_period_id" name="population_period_id"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="" selected>{{ __('Pilih periode') }}</option>
-                                @foreach ($periode as $item)
+                                @foreach (collect($periode)->sortByDesc('population_period_year') as $item)
                                     <option value="{{ $item['population_period_id'] }}">Tahun
                                         {{ $item['population_period_year'] }} | Semester
                                         {{ $item['population_period_semester'] }}</option>
