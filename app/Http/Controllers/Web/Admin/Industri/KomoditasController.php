588697afab774d9b20bc9d3e5f88b1ff62cee18d
upimhmdirfn <mhmdirfn01@gmail.com>
2025-03-03 09:32:33 +0700

    feat: menampilkan data komoditas ke table

--- app/Http/Controllers/Web/Admin/Industri/KomoditasController.php
+++ app/Http/Controllers/Web/Admin/Industri/KomoditasController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Web\Admin\Industri;
 
 use App\Http\Controllers\Controller;
+use App\Services\ApiClient;
+use Exception;
 use Illuminate\Http\Request;
 
 class KomoditasController extends Controller
 {
-    public function index() {
-        return view("admin.industri.komoditas.index"); // Update the path here
-    }
 
+    private $apiClient;
+
+    public function __construct()
+    {
+        $this->apiClient = new ApiClient(config("app.url") . "/api");
+    }
+    public function index(Request $request) {
+        $this->apiClient->setToken(request()->session()->get("auth_token"));
+
+        $filters = $request->only(["region", "skala"]);
+
+        try {
+            $data_price = $this->apiClient->get("/disperindag/price/data", $filters);
+
+            if ($data_price["status_code"] === 200 && isset($data_price["data_price"]) && is_array($data_price["data_price"])) {
+                return view("admin.industri.komoditas.index", [
+                    "data_price" => $data_price["data_price"],
+                ]);
+            }
+
+
+            throw new Exception("Terjadi kesalahan");
+        } catch (Exception $e) {
+            flash($e->getMessage(), "error");
+            return redirect()->back();
+        }
+    }
 }
--- resources/views/admin/industri/komoditas/index.blade.php
+++ resources/views/admin/industri/komoditas/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Data Komoditas')
 @section('content')
     <x-panel.panel>
-        <x-panel.panel-header title="{{ __('Data Industri Kecil Menengah') }}">
+        <x-panel.panel-header title="{{ __('Data Harga Komoditas') }}">
             <div class="flex items-center gap-2">
                 <button type="button" data-modal-target="import-modal" data-modal-toggle="import-modal"
                     class="md:inline-flex text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 text-center items-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
@@ -29,20 +29,62 @@
         <x-panel.panel-body>
             <div
                 class="overflow-x-auto rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03]">
-                <table id="default-table" class="min-w-full divide-y divide-gray-200">
+                <table id="default-table">
                     <thead>
                         <tr>
-                            <th class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">Kode
-                                BPS 2023</th>
-                            <th class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                Kabupaten Kota</th>
-                            <th class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">Nama
-                                Variant</th>
-                            <!-- Header tanggal akan ditambahkan secara dinamis oleh JavaScript -->
+                            <th>
+                                <span class="flex items-center">
+                                    Kode BPS 2023
+                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                    </svg>
+                                </span>
+                            </th>
+                            <th>
+                                <span class="flex items-center">
+                                    Kabupaten Kota
+                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                    </svg>
+                                </span>
+                            </th>
+                            <th>
+                                <span class="flex items-center">
+                                    Nama Variant
+                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                    </svg>
+                                </span>
+                            </th>
+                            <th>
+                                <span class="flex items-center">
+                                    Tanggal
+                                    <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
+                                    </svg>
+                                </span>
+                            </th>
+
                         </tr>
                     </thead>
-                    <tbody id="table-body">
-                        <!-- Data akan dimasukkan secara dinamis melalui JavaScript -->
+                    <tbody>
+                        @foreach ($data_price as $key => $dprice)
+        <tr>
+            <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $dprice['prices_value'] ?? '-' }}</td>
+            <td>{{ $dprice['region_name'] ?? '-' }}</td>
+            <td>{{ $dprice['variant_name'] ?? '-' }}</td>
+            <td>{{ $dprice['date'] ?? '-' }}</td>
+        </tr>
+    @endforeach
                     </tbody>
                 </table>
             </div>
@@ -65,69 +107,5 @@
 @endsection
 
 @push('scripts')
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const tableHeadRow = document.querySelector("thead tr");
-            const tableBody = document.getElementById("table-body");
-            const currentDate = new Date();
-            let dates = [];
 
-            // Mengambil 4 tanggal terbaru dalam format DD/MM/YY
-            for (let i = 0; i < 4; i++) {
-                let date = new Date();
-                date.setDate(currentDate.getDate() - (3 - i)); // Agar urutannya dari lama ke terbaru
-                let formattedDate = date.toLocaleDateString('id-ID', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: '2-digit'
-                });
-                dates.push(formattedDate);
-            }
-
-            // Menambahkan header tanggal ke tabel
-            dates.forEach(date => {
-                let th = document.createElement("th");
-                th.className =
-                    "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400";
-                th.innerText = date;
-                tableHeadRow.appendChild(th);
-            });
-
-            // Data contoh
-            const data = [{
-                    kode: "1701",
-                    kota: "Kab. Bengkulu Selatan",
-                    variant: "Bawang Bombai",
-                    prices: [38000, 38000, 38000, 38000]
-                },
-                {
-                    kode: "1701",
-                    kota: "Kab. Bengkulu Selatan",
-                    variant: "Bawang Merah",
-                    prices: [40000, 35000, 35000, 35000]
-                },
-                {
-                    kode: "1701",
-                    kota: "Kab. Bengkulu Selatan",
-                    variant: "Bawang Putih Honan",
-                    prices: [42000, 40000, 40000, 40000]
-                }
-            ];
-
-            // Menambahkan data ke tabel
-            data.forEach(item => {
-                let row = `<tr>
-            <td class="px-6 py-4">${item.kode}</td>
-            <td class="px-6 py-4">${item.kota}</td>
-            <td class="px-6 py-4">${item.variant}</td>
-            ${item.prices.map(price => `<td class="px-6 py-4">${price.toLocaleString('id-ID')}</td>`).join('')}
-        </tr>`;
-                tableBody.innerHTML += row;
-            });
-        });
-        document.addEventListener("DOMContentLoaded", function() {
-            const tableBody = document.getElementById("table-body");
-
-        });
-    </script>
 @endpush
