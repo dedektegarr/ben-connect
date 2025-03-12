91f0b90ee18686fb0806efd8394420b17655adbc
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2025-03-12 12:08:29 +0700

    debug

--- app/Http/Controllers/Web/Admin/Industri/IndustriNasionalController.php
+++ app/Http/Controllers/Web/Admin/Industri/IndustriNasionalController.php
@@ -43,34 +43,27 @@ class IndustriNasionalController extends Controller
     {
         $this->apiClient->setToken(request()->session()->get("auth_token"));
 
-        try {
-            $request->validate([
-                "file" => "required|file|mimes:xls,xlsx|max:5000",
-            ], [
-                "file.required" => "File data SIINas tidak boleh kosong",
-                'file.file' => 'Data SIInas harus berupa file',
-                'file.mimes' => 'File data SIInas harus berformat .xls atau .xlsx',
-                'file.max' => 'File data SIInas maksimal 5 Mb ',
-            ]);
+        $request->validate([
+            "file" => "required|file|mimes:xls,xlsx|max:5000",
+        ], [
+            "file.required" => "File data SIINas tidak boleh kosong",
+            'file.file' => 'Data SIInas harus berupa file',
+            'file.mimes' => 'File data SIInas harus berformat .xls atau .xlsx',
+            'file.max' => 'File data SIInas maksimal 5 Mb ',
+        ]);
 
-            $import = $this->apiClient->post("/disperindag/indusrty/import", [], $request->files);
+        $import = $this->apiClient->post("/disperindag/indusrty/import", [], $request->files);
 
-            if ($import["status"] === "error") {
-                flash($import["errors"], "error");
-                return redirect()->back();
-            }
-
-            if ($import["status"] === 500) {
-                throw new Exception($import["message"]);
-            }
-
-            flash("Data industri SIInas berhasil di import");
-            return redirect()->back();
-        } catch (ValidationException $e) {
-            return redirect()->back()->withErrors($e->errors());
-        } catch (Exception $e) {
-            flash($e->getMessage(), "error");
+        if ($import["status"] === "error") {
+            flash($import["errors"], "error");
             return redirect()->back();
         }
+
+        if ($import["status"] === 500) {
+            throw new Exception($import["message"]);
+        }
+
+        flash("Data industri SIInas berhasil di import");
+        return redirect()->back();
     }
 }
